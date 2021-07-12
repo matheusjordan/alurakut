20a7c27e86bd569baf1ef5e84a1ca61e08dcf9bd
Matheus Jordan <matheusj1599@gmail.com>
2021-07-12 12:50:55 -0300

    Inicio do Alurakut

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,66 @@
-import styled from 'styled-components'
+import MainGrid from '../src/Components/MainGrid'
+import Box from '../src/Components/Box';
+import ProfileRelationsBoxWrapper from '../src/Components/ProfileRelationsBoxWrapper';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/Lib/AlurakutCommons';
+
+const ProfilePicture = (props) => {
+  return (
+    <Box>
+      <img src={`https://github.com/${props.githubUser}.png`}></img>
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = 'matheusjordan';
+  const githubFriends = [
+    'juunegreiros',
+    'omariosouto',
+    'peas',
+    'rafaballerini',
+    'marcobrunodev',
+    'felipefialho'
+  ]
+
+  return (
+    <>
+      <AlurakutMenu/>
+      <MainGrid>
+        <div style={{ gridArea: 'profileArea'}}>
+          <ProfilePicture githubUser={githubUser}/>
+
+        </div>
+        <div style={{ gridArea: 'welcomeArea'}}>
+          <Box>
+            <h1 className="title">Bem Vindo(a)</h1>
+            <OrkutNostalgicIconSet/>
+          
+          </Box>
+        </div>
+        <div style={{ gridArea: 'profileRelationsArea'}}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Amigos da Comunidade ({githubFriends.length})
+            </h2>
+            <ul>
+              {githubFriends.map(friend => {
+                return (
+                  <li>
+                    <a href={`users/${friend}`} key={friend}>
+                      <img src={`https://github.com/${friend}.png`}/>
+                      <span>{friend}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <Box>Comunidades</Box>
+
+        </div>
+        
+      </MainGrid>
+    </>
+  )
 }
